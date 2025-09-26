813a975774c123452d12acd7f74ac245ce304ee0
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-09-27 02:15:15 +0300

    Migrate to API Tokens from Global Account Key
    Also some refactoring because wow that really was written in "somewhat of a hurry" for university.

--- README.md
+++ README.md
@@ -1,29 +1,44 @@
 # cloudflare-dynamic-dns
-A small script to automatically update the IP address(es) attached to a CloudFlare DNS record(s).
+A small NodeJS tool to automatically update the IP address of CloudFlare DNS records.
 
-This was written in somewhat of a hurry to allow a Raspberry PI to host some university projects.
+## Requirements
 
-## Configuration
+* Node v14+
+* Write access in the tool's own folder.
 
-The `config.json` file's `records` array can handle individual DNS records in this format:
+## Usage
+
+The tool can be either be run on a schedule (for example via cron) or on a NodeJS interval (See [`checkIntervalMS`](#checkIntervalMS)) once started.
+
+Cron example (check once every 2 hours) : `0 */2 * * * cd /opt/DNS && node index.js`.
+
+### CLI arguments
+
+`--config <file-path>` : Use a different configuration file than the default (`.\config.json`). When used, ensure that the script has write access to the file.
+
+`--verbose` : Logs every IP check, regardless if the address changed.
+
+### Configuration
+
+> [!WARNING]
+> Configuration files contain secrets and should be treated as confidential.
+
+To configure which records to update, fill out the `config.json` file:
 
 ```json
 {
-    "ip": "",
     "checkIntervalMS": 0,
-    "logging": "",
+    "token": "0000000000000000000000000000000000000",
     "records": [
         {
-            "authEmail": "example1@example.com",
-            "authKey": "0000000000000000000000000000000000000",
+            "token": "0000000000000000000000000000000000000",
             "zoneName": "example1.com",
             "zoneIdentifier": "",
             "recordName": "example1.com",
             "recordIdentifier": ""
         },
         {
-            "authEmail": "example2@example.com",
-            "authKey": "0000000000000000000000000000000000000",
+            "token": "0000000000000000000000000000000000000",
             "zoneName": "example2.com",
             "zoneIdentifier": "",
             "recordName": "example2.com",
@@ -32,19 +47,55 @@ The `config.json` file's `records` array can handle individual DNS records in th
     ]
 }
 ```
+#### `token`
 
-The `authEmail` field is the email that is registered with the domain, and `authKey` can be obtained from [https://dash.cloudflare.com/profile/api-tokens](https://dash.cloudflare.com/profile/api-tokens), in the "Global API Key" section.
+Optional; if all of the domain records belong to the same account, a single API Token is enough to manage them. This field's value will be treated as the default token but if a record specifies its own token, that will be used instead.
 
-The `zoneName` should be set to the root domain name of the website, while the `recordName` is the name of the A Type record.
+See [the API token documentation](https://developers.cloudflare.com/fundamentals/api/get-started/create-token/) to see how to create one.
 
-Both `zoneIdentifier` and `recordIdentifier` should be left empty, along with the `ip` field on the top of the file; these will be filled out by the script on startup.
+#### `records`
 
-The `checkIntervalMS` property can be used to set how often, in milliseconds, the script should check if the IP address has changed. By default it is set to 0 minutes, which disables this function. This is useful if the script is periodically called from another source (for example from cron).
+The records array contains the list of DNS records the script will manage. Some information will be automatically filled in on startup, so it is important the script has write access to this file.
 
-Once started, this script runs once - or if the `checkIntervalMS` property is set, indefinitely - and logs any issues into `./logs/log.txt`.
+##### `token`
 
-For full logging (write to logline everytime a check has been done, regardless if the IP changed), set `logging` to 'full'.
+The API token used to manage the record. If present, this token will be used when updating the record, regardless of the default token.
 
-## IP check
+Optional if the main token field already contains a token that is valid for this record.
 
-The script checks for the machine's IP address by sending a get request to a great external service, [https://www.ipify.org/](https://www.ipify.org/) <3.
+See [the API token documentation](https://developers.cloudflare.com/fundamentals/api/get-started/create-token/) to see how to create one.
+
+##### `zoneName`
+
+The base domain name where the record applies to. This is the purchased domain name such as "example.com", and is usually the same as the top `A` record's.
+
+Optional if `zoneIdentifier` is filled out from the dashboard; otherwise required and the script will automatically resolve it.
+
+##### `zoneIdentifier`
+
+The identifier of the zone. If not filled out `zoneName` will be used to resolve it and the script will write it to the file. To manually fill in the ID, [you can copy it from the main dashboard](https://developers.cloudflare.com/fundamentals/account/find-account-and-zone-ids/#copy-your-zone-id).
+
+##### `recordName`
+
+The name of the [`A` record](https://developers.cloudflare.com/dns/manage-dns-records/reference/dns-record-types/#a-and-aaaa) to update.
+
+##### `recordIdentifier`
+
+The identifier of the record. This will be automatically resolved from `recordName` and written to file.
+
+#### `checkIntervalMS`
+
+Setting this property will keep the script running and sets how often, in milliseconds, it should check if the IP address has changed. If set to 0 or omitted this is disabled.
+
+> [!NOTE]
+> Be considerate about the check frequency and set it to a reasonable interval to not spam the IP check service. Generally shouldn't be lower than 30 minutes, (`1800000`ms).
+
+## IP discovery
+
+The script uses [https://www.ipify.org/](https://www.ipify.org/) to check the server's [IPv4](https://api.ipify.org/) address. (They are great <3)
+
+The last known IP address is written to the `.\ipv4` file as plaintext. 
+
+## Logging
+
+Logs are written to `.\log.txt`.
--- index.js
+++ index.js
@@ -1,114 +1,183 @@
 #!/usr/bin/env node
 const axios = require('axios').default;
 const fs = require('fs');
+const path = require('path');
+const argv = require('process').argv;
 
-const configPath = './config.json';
-const logPath = './logs/log.txt';
+const logLevel = argv.includes("--verbose") ? "verbose" : "normal";
+const logPath = path.join(__dirname, 'log.txt');
+const ipFilePath = path.join(__dirname, 'ipv4');
 
+const defaultConfigPath = path.join(__dirname, 'config.json');
+const configPath = argv.includes("--config") ? argv[argv.indexOf("--config")+1] : defaultConfigPath;
 var config = JSON.parse(fs.readFileSync(configPath));
 
-const API_check_server = "https://api.ipify.org/?format=json"; // They are very cool <3
-const create_cloudflare_getZoneID_URL = (zoneName) => {return `https://api.cloudflare.com/client/v4/zones?name=${zoneName}`}
-const create_cloudflare_getRecordID_URL = (zoneID, recordName) => {return `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records?name=${recordName}`}
-const create_cloudflare_updateDNSrecord_URL = (zoneID, recordID) => {return `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records/${recordID}`}
+// Expects text/plain
+const IP_check_server_url = "https://api.ipify.org/"; // They are very cool <3
+
+const getCloudflareZoneIdURL = (zoneName) => {return `https://api.cloudflare.com/client/v4/zones?name=${zoneName}`}
+const getCloudflateRecordIdURL = (zoneID, recordName) => {return `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records?name=${recordName}`}
+const getCloudflareRecordURL = (zoneID, recordID) => {return `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records/${recordID}`}
 
 var updateCheckInterval = null;
+var ip = "";
 
-function log (message, description="") {
-	const logline = `${new Date().toUTCString()} - ${description}:\n${message}\n`;
-	fs.appendFileSync(logPath, logline);
+function log(event, description = "") {
+    const logline = `${new Date().toUTCString()} - ${event}${description.length == 0 ? `.` : ` :\n${description}`}\n`;
+    console.log(logline);
+    if (!fs.existsSync(logPath))
+    {
+        fs.writeFileSync(logPath, logline);
+    }
+    else {
+        fs.appendFileSync(logPath, logline);
+    }
 }
 
 async function start() {
-	if (!fs.existsSync(logPath))
-	{
-		fs.writeFileSync(logPath, "");
-		log("Log file created", "Initialisation");
-	}
-    if (config.checkIntervalMS > 0)
+    if (config.checkIntervalMS != undefined && config.checkIntervalMS > 0)
     {
-        updateCheckInterval = setInterval(checkIP, config.checkIntervalMS);
-        log(`Script started with native interval of ${config.checkIntervalMS}ms`, "Startup");
+        updateCheckInterval = setInterval(update, config.checkIntervalMS);
+        log(`Script started with native interval of ${config.checkIntervalMS}ms`);
     }
-    // Wait until all the IDs are received.
-    await getIDs();
-    checkIP();
+    await resolveIdentifiers();
+    update();
 }
 
-function checkIP () {
-	axios.get(API_check_server).then(res => {
-		if (config.ip != res.data.ip)
-		{
-			// IP changed, update DNS records
-			config.ip = res.data.ip;
-			log(`New IP: ${config.ip}`, "IP updated");
-			updateConfig();
-		}
-		else if (config.logging == 'full') {
-			log('IP not changed');
-		}
-	}).catch(error => {
-		log(error, "IP check failed");
-	})
+async function update() {
+    const changed = await checkIP();
+    if (changed) {
+        for (record of config.records) {
+            await updateRecord(record);
+        }
+    }
 }
 
-async function getIDs () {
-	for (const [index, record] of config.records.entries())
-	{
-		let zoneID = "";
-		let recordID = "";
-		if (record.recordIdentifier == "" || record.zoneIdentifier == "")
-		{
-			const cloudflare_headers = {
-				'Content-Type': 'application/json',
-				'X-Auth-Email': record.authEmail,
-				'X-Auth-Key': record.authKey
-			}
-			// Get the request url for the Zone ID
-			const cloudflareZoneID_URL = create_cloudflare_getZoneID_URL(record.zoneName);
-			await axios.get(cloudflareZoneID_URL, {headers: cloudflare_headers}).then(res => {
-				zoneID = res.data.result[0].id;
-				config.records[index].zoneIdentifier = zoneID;
-			}).catch(error => {
-				log(error, "Zone ID request failed");
-			});
-			// Get the request url for the Record ID
-			const cloudflareRecordID_URL = create_cloudflare_getRecordID_URL(zoneID, record.recordName);
-			await axios.get(cloudflareRecordID_URL, {headers: cloudflare_headers}).then(res => {
-				recordID = res.data.result[0].id;
-				config.records[index].recordIdentifier = recordID;
-			}).catch(error => {
-				log(error, "Record ID request failed");
-			});
-		}
-	};
+async function checkIP() {
+    const old = fs.existsSync(ipFilePath) ? fs.readFileSync(ipFilePath) : "unknown";
+    try {
+        const res = await axios.get(IP_check_server_url);
+        const current = res.data;
+        if (old != current) {
+            log(`New IP ${current}`, `${old} -> ${current}`);
+            ip = current;
+            fs.writeFileSync(ipFilePath, current);
+            return true;
+        }
+        else {
+            if (logLevel == 'verbose') {
+                log('IP not changed');
+            }
+        }
+    }
+    catch (error) {
+        if (error.response) {
+            log("IP check failed", `${error.response.status} - ${error.response.data}`);
+        }
+        else if (error.request) {
+            log("IP check failed", `Request failed.`);
+        }
+        else {
+            log("IP check failed", error.message);
+        }
+    }
+    return false;
 }
 
-function updateConfig() {
-	fs.writeFileSync(configPath, JSON.stringify(config, null, "\t"));
-	updateDNS();
+/**
+ * Resolves Cloudflare Zone and Record IDs if they are not in the config file.
+ */
+async function resolveIdentifiers() {
+    for (record of config.records) {
+        let zoneID = record.zoneIdentifier;
+        const headers = getAuthHeader(record);
+        if (record.zoneIdentifier == "" || record.zoneIdentifier == undefined)
+        {
+            const cloudflareZoneID_URL = getCloudflareZoneIdURL(record.zoneName);
+            try {
+                const res = await axios.get(cloudflareZoneID_URL, { headers: headers });
+                zoneID = res.data.result[0].id;
+                record.zoneIdentifier = zoneID;
+                log(`Zone ID resolved for ${record.zoneName}`, `${record.zoneName} -> ${zoneID}`);
+            }
+            catch (error) {
+                if (error.response) {
+                    log(`Failed to resolve Zone ID for ${record.zoneName}`, `${error.response.status} - ${JSON.stringify(error.response.data, null, "\t")}`);
+                }
+                else if (error.request) {
+                    log(`Failed to resolve Zone ID for ${record.zoneName}`, `No response received.`);
+                }
+                else {
+                    log(`Failed to resolve Zone ID for ${record.zoneName}`, error.message);
+                }
+            }
+        }
+        if (record.recordIdentifier == "" || record.recordIdentifier == undefined) {
+            const cloudflareRecordID_URL = getCloudflateRecordIdURL(zoneID, record.recordName);
+            try {
+                const res = await axios.get(cloudflareRecordID_URL, { headers: headers });
+                let recordID = res.data.result[0].id;
+                record.recordIdentifier = recordID;
+                log(`Record ID resolved for ${record.recordName}`, `${record.recordName} -> ${recordID}`);
+            }
+            catch (error) {
+                if (error.response) {
+                    log(`Failed to resolve Record ID for ${record.recordName}`, `${error.response.status} - ${JSON.stringify(error.response.data, null, "\t")}`);
+                }
+                else if (error.request) {
+                    log(`Failed to resolve Record ID for ${record.recordName}`, `No response received.`);
+                }
+                else {
+                    log(`Failed to resolve Record ID for ${record.recordName}`, error.message);
+                }
+            }
+        }
+    }
+    // Write out the identifiers back to the config file
+    fs.writeFileSync(configPath, JSON.stringify(config, null, "\t"));
 }
 
-function updateDNS() {
-	config.records.forEach(record => {
-		const cloudflareUpdateDNS_URL = create_cloudflare_updateDNSrecord_URL(record.zoneIdentifier, record.recordIdentifier);
-		const cloudflareUpdate = {
-			id: record.zoneIdentifier,
-			type: "A",
-			name: record.recordName, 
-			content: config.ip
-		}
-		const cloudflare_headers = {
-			'Content-Type': 'application/json',
-			'X-Auth-Email': record.authEmail,
-			'X-Auth-Key': record.authKey
-		}
-		axios.patch(cloudflareUpdateDNS_URL, cloudflareUpdate, {headers: cloudflare_headers}).then(res => {
-			log(`DNS Record updated for ${record.recordName}.`, `New IP: ${config.ip}.`);
-		}).catch(error => {
-			log(error, `DNS Record update failed for ${record.recordName}`);
-		});
-	});
+/**
+ * Updates the given DNS A record with the current IP.
+ * @param {{token?:string, zoneName?: string, zoneIdentifier:string, recordName:string, recordIdentifier:string, type:"A"|"AAAA" }} record 
+ */
+async function updateRecord(record) {
+    const cloudflareUpdateDNS_URL = getCloudflareRecordURL(record.zoneIdentifier, record.recordIdentifier);
+    const body = {
+        id: record.zoneIdentifier,
+        type: "A",
+        name: record.recordName, 
+        content: ip
+    }
+    const headers = getAuthHeader(record);
+    try {
+        await axios.patch(cloudflareUpdateDNS_URL, body, { headers: headers });
+        log(`DNS Record updated for ${record.recordName}.`, `New IP: ${ip}.`);
+    }
+    catch (error) {
+        if (error.response) {
+            log(`DNS Record update failed for ${record.recordName}`, `${error.response.status} - ${JSON.stringify(error.response.data, null, "\t")}`);
+        }
+        else if (error.request) {
+            log(`DNS Record update failed for ${record.recordName}`, `No response received.`);
+        }
+        else {
+            log(`DNS Record update failed for ${record.recordName}`, error.message);
+        }
+    }
+}
+
+/**
+ * Retruns the auth header to the given record. Uses either the record's own token, or the global one.
+ * @param {{token?:string}} record 
+ * @returns 
+ */
+function getAuthHeader(record) {
+    const token = record.token ?? config.token;
+    return {
+        'Content-Type': 'application/json',
+        'Authorization' : `Bearer ${token}`
+    }
 }
 
 start();
